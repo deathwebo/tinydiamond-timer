b0621cb2cca1ed331fc0f8226bcc9947c2d2a078
Erick M <tony3c24@gmail.com>
2020-10-22 19:52:39 -0700

    Add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // purge: { //ENABLE TO TEST LOCALLY
+    //   enabled: true,
+    //   content: ['./public/**/*.html', './src/**/*.html', './src/**/*.vue',],
+    // },
+    purge: [
+        './public/**/*.html',
+        './src/**/*.html',
+        './src/**/*.vue',
+    ],
+}
