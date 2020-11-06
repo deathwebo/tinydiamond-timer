6adb35cb3460e25c66bf3b720836f68cb1ce70c5
Erick M <tony3c24@gmail.com>
2020-11-05 20:56:34 -0800

    Validate comma character

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,28 +6,36 @@
 
     <div class="flex mb-6 justify-evenly">
       <div class="flex-1 text-right pr-6">
-        <div class="inline-flex items-center border-b border-teal-500 py-2">
-          <input
-            class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-            type="text"
-            v-model="newMember"
-            v-on:keyup="validateEnter"
-            placeholder="Add member"
-            aria-label="Full name"
-          />
-          <button
-            @click="addNewMember"
-            class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-            type="button"
+        <div class="inline-flex flex-col">
+          <div class="items-center border-b py-2" :class="inputExtraClass">
+            <input
+              class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              v-model="newMember"
+              v-on:keyup="validateEnter"
+              placeholder="Add member"
+              aria-label="Full name"
+            />
+            <button
+              @click="addNewMember"
+              class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="button"
+            >
+              Add
+            </button>
+            <button
+              class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+              type="button"
+            >
+              Clear
+            </button>
+          </div>
+          <p
+            v-if="invalidCharacters"
+            class="text-red-500 text-xs italic text-left mt-1"
           >
-            Add
-          </button>
-          <button
-            class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
-            type="button"
-          >
-            Clear
-          </button>
+            No comma characters allowed
+          </p>
         </div>
         <div class="mt-4">
           <span class="text-gray-500 mr-2">Duration per member in seconds</span>
@@ -85,6 +93,21 @@ export default {
       timerDuration: 90
     };
   },
+  computed: {
+    inputExtraClass() {
+      if (this.invalidCharacters) {
+        return "border-red-500";
+      } else {
+        return "border-teal-500";
+      }
+    },
+    invalidCharacters() {
+      if (this.newMember != "" && this.newMember.includes(",")) {
+        return true;
+      }
+      return false;
+    }
+  },
   created() {
     const timerDuration = parseInt(localStorage.getItem("timerDuration"));
     if (Number.isInteger(timerDuration)) {
@@ -102,7 +125,11 @@ export default {
   },
   methods: {
     addNewMember() {
-      if (this.newMember != "" && !this.members.includes(this.newMember)) {
+      if (
+        this.newMember != "" &&
+        !this.invalidCharacters &&
+        !this.members.includes(this.newMember)
+      ) {
         this.members = this.members.concat(this.newMember);
         this.newMember = "";
       }
